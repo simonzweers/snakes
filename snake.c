a7dc74271d703ed7fa60be1e9de3c44af0721927
Simon Zweers <s.zweers21@gmail.com>
2024-05-15 02:59:15 +0200

    Implemented most snake logic methods, still has a double free

--- snake.c
+++ snake.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <ncurses.h>
 #include <unistd.h>
+#include <stdlib.h>
 
 const int FIELD_SIZE_X = 30;
 const int FIELD_SIZE_Y = 20;
@@ -17,6 +18,68 @@ typedef struct snake_head_pos
 	int y_pos;
 } snake_head_pos_t;
 
+typedef struct snake_node
+{
+	int x;
+	int y;
+	struct snake_node *next;
+} snake_node_t;
+
+typedef struct snake
+{
+	snake_node_t* head;
+} snake_t;
+
+snake_t* snake_new(snake_head_pos_t *snake_head_pos) {
+	snake_t* new_snake = (snake_t*) malloc(sizeof(snake_t));
+	new_snake->head = (snake_node_t*) malloc(sizeof(snake_node_t));
+	new_snake->head->next = NULL;
+	new_snake->head->x = snake_head_pos->x_pos;
+	new_snake->head->y = snake_head_pos->y_pos;
+	printw("Created new snake %p\n", new_snake);
+	return new_snake;
+}
+
+void snake_add_node(snake_t *snake, snake_head_pos_t *snake_head_pos) {
+	snake_node_t *new_node = (snake_node_t *) malloc(sizeof(snake_node_t));
+	new_node->x = snake_head_pos->x_pos;
+	new_node->y = snake_head_pos->y_pos;
+	new_node->next = snake->head;
+	snake->head = new_node;
+	printw("Adding node: %p | x: %d, y: %d\n", new_node, new_node->x, new_node->y);
+}
+
+void snake_remove_last(snake_t *snake) {
+	snake_node_t *cursor = snake->head;
+	snake_node_t *cursor_previous;
+	while (cursor->next != NULL)
+	{
+		cursor_previous = cursor;
+		cursor = cursor->next;
+	}
+	cursor_previous->next = NULL;
+	printw("Freeing last node: %p | x: %d, y: %d\n", cursor, cursor->x, cursor->y);
+	free(cursor);
+}
+
+void snake_propogate(snake_t *snake, snake_head_pos_t *snake_head_pos) {
+	snake_add_node(snake, snake_head_pos);
+	snake_remove_last(snake);
+}
+
+void snake_free(snake_t *snake) {
+	snake_node_t *cursor = snake->head;
+	snake_node_t *cursor_next;
+	while (cursor != NULL)
+	{
+		cursor_next = cursor->next;
+		printw("Freeing node %p | x: %d, y: %d\n", cursor, cursor->x, cursor->y);
+		free(cursor);
+		cursor = cursor_next;
+	}
+	printw("freeing snake %p\n", snake);
+	free(snake);
+}
 
 void parse_input(int input_char, snake_dir_t* snake_dir_ptr) {
 	// Y coordinate is inverted because of terminal coordinates
@@ -60,7 +123,7 @@ bool is_valid_position(int x, int y) {
 int main() {
     WINDOW* win = initscr();
 	keypad(win, true);
-	nodelay(win, true);
+	// nodelay(win, true);
 	snake_head_pos_t snake_head_pos = {
 		.x_pos = FIELD_SIZE_X / 2,
 		.y_pos = FIELD_SIZE_Y / 2,
@@ -69,20 +132,41 @@ int main() {
 		.x_dir = -1,
 		.y_dir = 0,
 	};
-	while (1) {
-		int chr = getch();
-		parse_input(chr, &snake_dir);
-		set_new_pos(&snake_head_pos, &snake_dir);
-		if (!is_valid_position(snake_head_pos.x_pos, snake_head_pos.y_pos))
-		{
-			break;
-		}
+	// while (1) {
+	// 	int chr = getch();
+	// 	parse_input(chr, &snake_dir);
+	// 	set_new_pos(&snake_head_pos, &snake_dir);
+	// 	snake_propogate(&snake_head_pos);
+	// 	if (!is_valid_position(snake_head_pos.x_pos, snake_head_pos.y_pos))
+	// 	{
+	// 		break;
+	// 	}
+
+	// 	if (is_food_pos())
+	// 	{
+	// 		snake_add_node();
+	// 	} else
+	// 	{
+	// 		snake_propogate();
+	// 	}
 		
-		erase();
-		mvaddstr(snake_head_pos.y_pos, snake_head_pos.x_pos * 2, "##");
-		usleep(100 * 1000);
-	}
+		
+		
+	// 	erase();
+	// 	mvaddstr(snake_head_pos.y_pos, snake_head_pos.x_pos * 2, "##");
+	// 	usleep(100 * 1000);
+	// }
+	snake_t *snake= snake_new(&snake_head_pos);
+	snake_head_pos.x_pos = 1; snake_head_pos.y_pos = 2;
+	// snake_add_node(snake, &snake_head_pos);
+	// snake_head_pos.x_pos = 3; snake_head_pos.y_pos = 4;
+	// snake_add_node(snake, &snake_head_pos);
+	// snake_head_pos.x_pos = 5; snake_head_pos.y_pos = 6;
+	// snake_add_node(snake, &snake_head_pos);
+	snake_remove_last(snake);
+	snake_free(snake);
 	
+	getch();
 	endwin();			/* End curses mode		  */
 
 	return 0;
