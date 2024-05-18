bd2ae0c838c5a3156587c8c833587d22356387a9
Simon Zweers <s.zweers21@gmail.com>
2024-05-18 19:02:46 +0200

    Catch ctrl C signal

--- go/main.go
+++ go/main.go
@@ -2,14 +2,16 @@ package main
 
 import (
 	"log"
+	"os"
+	"os/signal"
+	"time"
 
 	"github.com/rthornton128/goncurses"
 	"github.com/simonzweers/snakes/go/snakego/display"
-	"github.com/simonzweers/snakes/go/snakego/snake"
 )
 
 func main() {
-	snake.SnakeInit()
+	// Init ncurses
 	stdscr, err := goncurses.Init()
 	if err != nil {
 		log.Fatal("init:", err)
@@ -19,7 +21,20 @@ func main() {
 	goncurses.Echo(false) // turn echoing of typed characters off
 	stdscr.Keypad(true)   // allow keypad input
 
-	display.DisplayField(stdscr)
-	stdscr.Refresh()
-	stdscr.GetChar()
+	// Catch Ctrl-C
+	var gameloop bool = true
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	go func() {
+		<-c
+		gameloop = false
+	}()
+
+	// Init snake logic
+
+	for gameloop {
+		time.Sleep(100 * 1000)
+		display.DisplayField(stdscr)
+		stdscr.Refresh()
+	}
 }
