953b485f2029cf5fc81ed59eb63bd2ecf773f62c
Simon Zweers <simon.zweers@protonmail.com>
2025-04-27 20:55:51 +0200

    Create vaxis example

--- zig/src/main.zig
+++ zig/src/main.zig
@@ -1,21 +1,95 @@
 const std = @import("std");
+
 const snake = @import("snake");
+const vaxis = @import("vaxis");
+const Cell = vaxis.Cell;
+const TextInput = vaxis.widgets.TextInput;
+const border = vaxis.widgets.border;
+
+const Event = union(enum) {
+    key_press: vaxis.Key,
+    winsize: vaxis.Winsize,
+    focus_in,
+    foo: u8,
+};
 
 pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
-
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
     snake.hello();
 
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
+    // Initialize allocator
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer {
+        const deinit_status = gpa.deinit();
+        if (deinit_status == .leak) {
+            std.log.err("Memory Leak", .{});
+        }
+    }
+    const alloc = gpa.allocator();
 
-    try bw.flush(); // Don't forget to flush!
+    // Initialize a tty
+    var tty = try vaxis.Tty.init();
+    defer tty.deinit();
+
+    // Initialize vaxis
+    var vx = try vaxis.init(alloc, .{});
+    defer vx.deinit(alloc, tty.anyWriter());
+    var loop: vaxis.Loop(Event) = .{
+        .tty = &tty,
+        .vaxis = &vx,
+    };
+    try loop.init();
+    try loop.start();
+    defer loop.stop();
+
+    try vx.enterAltScreen(tty.anyWriter());
+    var color_idx: u8 = 0;
+
+    var text_input = TextInput.init(alloc, &vx.unicode);
+    defer text_input.deinit();
+
+    try vx.queryTerminal(tty.anyWriter(), 1 * std.time.ns_per_s);
+
+    while (true) {
+        const event = loop.nextEvent();
+        switch (event) {
+            .key_press => |key| {
+                color_idx = switch (color_idx) {
+                    255 => 0,
+                    else => color_idx + 1,
+                };
+                if (key.matches('c', .{.ctrl = true})) {
+                    break;
+                } else if (key.matches('l', .{.ctrl = true})) {
+                    vx.queueRefresh();
+                } else {
+                    try text_input.update(.{ .key_press = key});
+                }
+            },
+            .winsize => |ws| try vx.resize(alloc, tty.anyWriter(), ws),
+            else => {},
+        }
+        const win = vx.window();
+        win.clear();
+
+        const style: vaxis.Style = . {
+            .fg = .{ .index = color_idx},
+        };
+
+        const child = win.child(.{
+            .x_off = win.width / 2 - 20,
+            .y_off = win.height / 2-3,
+            .width = 40,
+            .height = 3,
+            .border = .{
+                .where = .all,
+                .style = style,
+            }
+        });
+
+        text_input.draw(child);
+        try vx.render(tty.anyWriter());
+    }
+
 }
 
 test "use other module" {
