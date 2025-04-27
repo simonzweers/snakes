3d95be781dad512edf928ebeef5dfa8b0cb72641
Simon Zweers <simon.zweers@protonmail.com>
2025-04-27 16:33:42 +0200

    Move imports up

--- zig/src/main.zig
+++ zig/src/main.zig
@@ -1,6 +1,5 @@
-//! By convention, main.zig is where your main function lives in the case that
-//! you are building an executable. If you are making a library, the convention
-//! is to delete this file and start with root.zig instead.
+const std = @import("std");
+const lib = @import("zig_lib");
 
 pub fn main() !void {
     // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
@@ -40,7 +39,3 @@ test "fuzz example" {
     try std.testing.fuzz(Context{}, Context.testOne, .{});
 }
 
-const std = @import("std");
-
-/// This imports the separate module containing `root.zig`. Take a look in `build.zig` for details.
-const lib = @import("zig_lib");
